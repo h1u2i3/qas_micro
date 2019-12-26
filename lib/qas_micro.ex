381de55d52764a808a1050251760140210badd79
Xiaohui Gan <286390860@qq.com>
2019-12-26 16:59:50 +0800

    remove unused variable warning & change new to new()

--- lib/qas_micro.ex
+++ lib/qas_micro.ex
@@ -1,21 +1,2 @@
 defmodule QasMicro do
-  require Logger
-
-  @doc """
-  Generate codes for app
-  """
-  def generate(app) do
-  end
-
-  @doc """
-  Remove qas api database
-  """
-  def remove_db(app) do
-  end
-
-  @doc """
-  Seed qas api database
-  """
-  def seed_db(app) do
-  end
 end
