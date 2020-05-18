ee19ac1986ce02e9e4cbd50ec3745a9dedf499bb
Xiaohui Gan <286390860@qq.com>
2020-05-18 21:39:04 +0800

    make custom json type support for string input

--- lib/qas_micro/json.ex
+++ lib/qas_micro/json.ex
@@ -4,6 +4,14 @@ defmodule QasMicro.Json do
   def type, do: :map
 
   def cast(map) when is_map(map), do: {:ok, map}
+
+  def cast(string) when is_binary(string) do
+    case Jason.decode(string, keys: :atoms) do
+      {:ok, map} -> {:ok, map}
+      {:error, _} -> :error
+    end
+  end
+
   def cast(_), do: :error
 
   def load(data) when is_map(data), do: Jason.encode(data)
