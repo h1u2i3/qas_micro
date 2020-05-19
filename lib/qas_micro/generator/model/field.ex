49efa556d935eb87ecac727da8fdaeb04f7f930b
Xiaohui Gan <286390860@qq.com>
2020-05-19 14:20:08 +0800

    use QasMicro.JsonArray to do cast type

--- lib/qas_micro/generator/model/field.ex
+++ lib/qas_micro/generator/model/field.ex
@@ -122,7 +122,7 @@ defmodule QasMicro.Generator.Model.Field do
       :geometry -> QMap.put(acc, :type, Geo.PostGIS.Geometry)
       type when type in [:text] -> QMap.put(acc, :type, :string)
       type when type in [:file, :json] -> QMap.put(acc, :type, QasMicro.Json)
-      type when type in [:jsons, :files] -> QMap.put(acc, :type, {:array, QasMicro.Json})
+      type when type in [:jsons, :files] -> QMap.put(acc, :type, QasMicro.JsonArray)
       _ -> QMap.put(acc, :type, type)
     end
   end
--- lib/qas_micro/json.ex
+++ lib/qas_micro/json.ex
@@ -18,3 +18,26 @@ defmodule QasMicro.Json do
 
   def dump(map) when is_map(map), do: {:ok, map}
 end
+
+defmodule QasMicro.JsonArray do
+  @behaviour Ecto.Type
+
+  def type, do: {:array, :map}
+
+  def cast(map) when is_map(map), do: {:ok, [map]}
+
+  def cast(map) when is_list(map), do: {:ok, map}
+
+  def cast(string) when is_binary(string) do
+    case Jason.decode(string, keys: :atoms) do
+      {:ok, map} -> cast(map)
+      {:error, _} -> :error
+    end
+  end
+
+  def cast(_), do: :error
+
+  def load(data) when is_list(data), do: Jason.encode(data)
+
+  def dump(map) when is_list(map), do: {:ok, map}
+end
