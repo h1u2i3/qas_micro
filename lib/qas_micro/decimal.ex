82067510f217e5ed32d269a3b82eaf5d56b2a3e1
Xiaohui Gan <286390860@qq.com>
2020-07-19 18:42:55 +0800

    fix: fix QasMicro.Decimal Ecto type issue

--- lib/qas_micro/decimal.ex
+++ lib/qas_micro/decimal.ex
@@ -4,13 +4,12 @@ defmodule QasMicro.Decimal do
   alias Decimal, as: D
 
   def type, do: :decimal
-  def cast(string) when is_binary(string), do: {:ok, D.new(string)}
-
-  def cast(%Decimal{} = decimal), do: {:ok, decimal}
-
+  def cast(string) when is_binary(string), do: {:ok, string}
+  def cast(%Decimal{} = decimal), do: {:ok, D.to_string(decimal)}
   def cast(_), do: :error
 
   def load(data), do: {:ok, D.to_string(data)}
 
-  def dump(decimal), do: {:ok, decimal}
+  def dump(string) when is_binary(string), do: {:ok, D.new(string)}
+  def dump(%Decimal{} = decimal), do: {:ok, decimal}
 end
